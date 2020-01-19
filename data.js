6454c2b29861c2826c2948e8c464230ff31be8e2
Avishai Elia <42171914+avishome@users.noreply.github.com>
2020-01-19 11:52:36 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,80 +1,81 @@
-[{
-		'title': 'A new Telegram Bot For the students of Lev Academic Center',
-		'link': 'https://github.com/avishome/TestsRoomBot',
-		'data': 'This is an advanced system which provides the end-users, Telegram reminders messages of scheduled coming exams. The system function completely automatically, and currently serves 250 students.',
-		'img': 'url(https://www.pc.co.il/wp-content/uploads/2015/11/Telegram_logo.svg_.jpg)',
-		'tech': [
-			"Python",
-			"Google Clude",
-			"Telegram",
-			"MongoDB"
-		],
-		'publishDate': "24.05.2019",
-		'demo': [
-			"1.jpg",
-			"2.jpg",
-			"3.jpg"
-		]
-	},
-	{
-		'title': 'Designer Apparel Store',
-		'link': 'https://avishome.github.io/mit/m.html',
-		'data': 'A shopping site, designed as an app that allows apparel designers to advertise their clothes.',
-		'img': 'url(https://avishome.github.io/mit/logo.png)',
-		'tech': [
-			"Html",
-			"Css",
-			"JavaScript",
-			"python",
-			"Google Func"
-		],
-		'publishDate': "10.02.2019",
-		'demo': [
-			"5.png",
-			"6.png"
-		]
-	},
-	{
-		'title': 'Driving Test scheduling system',
-		'link': 'https://github.com/avishome/WPF_TestSystem_2018',
-		'data': 'The system was created as a project during my studies. It was written in C#. The graphic interface was written in WPF. The system has a permission system similar to these of Linux',
-		'img': 'url(https://3.bp.blogspot.com/-_EKyDDZRYcY/WRfiFzlBuxI/AAAAAAAABuc/7mbh14-At8whTma-7HSvc50tAKkCaR9NACPcB/s1600/csharp.png)',
-		'tech': [
-			"C#",
-			"Wpf",
-			"Xml"
-		],
-		'publishDate': "10.02.2019",
-		'demo': [
-			"7.png"
-		]
-	},
-	{
-		'title': 'Bootstrap Generator',
-		'link': 'https://codepen.io/rbbxhpcj/pen/vQEEVZ',
-		'data': 'This is a client-side program that allows to create themes in Bootstrap easily and intuitively. The Generator was built using JQuery.',
-		'img': 'url(http://talmon.productions/wp-content/uploads/2015/02/bootstrap.png)',
-		'tech': [
-			"Jquery",
-			"Bootstrap 4"
-		],
-		'publishDate': "20.11.2018",
-		'demo': [
-			"4.png"
-		]
-	},
-	{
-		'title': 'Graphic engine',
-		'link': 'http://spark-graphic-e.herokuapp.com',
-		'data': '',
-		'img': 'url()',
-		'tech': [
-			"Jquery",
-			"Java"
-		],
-		'publishDate': "1.07.2019",
-		'demo': [
+[
+   {
+      "title":"A new Telegram Bot For the students of Lev Academic Center",
+      "link":"https://github.com/avishome/TestsRoomBot",
+      "data":"This is an advanced system which provides the end-users, Telegram reminders messages of scheduled coming exams. The system function completely automatically, and currently serves 250 students.",
+      "img":"url(https://www.pc.co.il/wp-content/uploads/2015/11/Telegram_logo.svg_.jpg)",
+      "tech":[
+         "Python",
+         "Google Clude",
+         "Telegram",
+         "MongoDB"
+      ],
+      "publishDate":"24.05.2019",
+      "demo":[
+         "1.jpg",
+         "2.jpg",
+         "3.jpg"
+      ]
+   },
+   {
+      "title":"Designer Apparel Store",
+      "link":"https://avishome.github.io/mit/m.html",
+      "data":"A shopping site, designed as an app that allows apparel designers to advertise their clothes.",
+      "img":"url(https://avishome.github.io/mit/logo.png)",
+      "tech":[
+         "Html",
+         "Css",
+         "JavaScript",
+         "python",
+         "Google Func"
+      ],
+      "publishDate":"10.02.2019",
+      "demo":[
+         "5.png",
+         "6.png"
+      ]
+   },
+   {
+      "title":"Driving Test scheduling system",
+      "link":"https://github.com/avishome/WPF_TestSystem_2018",
+      "data":"The system was created as a project during my studies. It was written in C#. The graphic interface was written in WPF. The system has a permission system similar to these of Linux",
+      "img":"url(https://3.bp.blogspot.com/-_EKyDDZRYcY/WRfiFzlBuxI/AAAAAAAABuc/7mbh14-At8whTma-7HSvc50tAKkCaR9NACPcB/s1600/csharp.png)",
+      "tech":[
+         "C#",
+         "Wpf",
+         "Xml"
+      ],
+      "publishDate":"10.02.2019",
+      "demo":[
+         "7.png"
+      ]
+   },
+   {
+      "title":"Bootstrap Generator",
+      "link":"https://codepen.io/rbbxhpcj/pen/vQEEVZ",
+      "data":"This is a client-side program that allows to create themes in Bootstrap easily and intuitively. The Generator was built using JQuery.",
+      "img":"url(http://talmon.productions/wp-content/uploads/2015/02/bootstrap.png)",
+      "tech":[
+         "Jquery",
+         "Bootstrap 4"
+      ],
+      "publishDate":"20.11.2018",
+      "demo":[
+         "4.png"
+      ]
+   },
+   {
+      "title":"Graphic engine",
+      "link":"http://spark-graphic-e.herokuapp.com",
+      "data":"",
+      "img":"url()",
+      "tech":[
+         "Jquery",
+         "Java"
+      ],
+      "publishDate":"1.07.2019",
+      "demo":[
 
-		]
-	}
-];
+      ]
+   }
+]
